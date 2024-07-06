ddec056e3926001478aebabb37f892f0fbc9df28
moniwero <mmonisiabb@gmail.com>
2024-07-06 10:26:16 +0200

    section 2

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 /**
  * Path to exercise folder
  */
-const exercisePath = "01_Car_Sharing";
+const exercisePath = "01_Best_Shop";
 
 /**
  * Don't change those lines below
